2f3b0756474daa143500936ccfe66ad4cfbdb51b
bipin101 <bipin.dubey@neosofttech.com>
2017-07-07 18:32:29 +0530

    Another One

---
+++ app/src/main/java/com/example/webwerks/another/Othr.java
@@ -0,0 +1,10 @@
+package com.example.webwerks.another;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by webwerks on 7/7/17.
+ */
+
+public class Othr extends AppCompatActivity {
+}
